01574fd70e15db2b1cca944a8637ac9c430954a4
Konst <konst.partnership@gmail.com>
2024-03-16 15:44:24 +0300

    Изменения в AppDrawer.kt: Добавлен переключатель темы со светлой на темную.

--- app/src/main/java/com/topic2/android/notes/ui/components/AppDrawer.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/AppDrawer.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -65,7 +66,7 @@ private fun ScreenNavigationButton(
 ) {
     val colors = MaterialTheme.colors
 
-    val imageVector = if (isSelected) {
+    val imageAlpha = if (isSelected) {
         1f
     } else {
         0.6f
@@ -84,8 +85,8 @@ private fun ScreenNavigationButton(
     }
     Surface(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 8.dp, end = 8.dp, top = 8.dp),
+                .fillMaxWidth()
+                .padding(start = 8.dp, end = 8.dp, top = 8.dp),
         color = backgroundColor,
         shape = MaterialTheme.shapes.small
     ) {
@@ -93,14 +94,15 @@ private fun ScreenNavigationButton(
             horizontalArrangement = Arrangement.Start,
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .clickable(onClick = onClick)
-                .fillMaxWidth()
-                .padding(4.dp)
+                    .clickable(onClick = onClick)
+                    .fillMaxWidth()
+                    .padding(4.dp)
         ) {
             Image(
                 imageVector = icon,
                 contentDescription = "Screen Navigation Button",
-                colorFilter = ColorFilter.tint(textColor)
+                colorFilter = ColorFilter.tint(textColor),
+                alpha = imageAlpha
             )
             Spacer(Modifier.width(16.dp))
             Text(
@@ -112,3 +114,48 @@ private fun ScreenNavigationButton(
         }
     }
 }
+
+@Composable
+private fun LightDarkThemeItem() {
+    Row(
+            Modifier.padding(8.dp)
+    ) {
+        Text(
+                text = "Включить темную тему",
+                style = MaterialTheme.typography.body2,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
+                modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp)
+                        .align(alignment = Alignment.CenterVertically)
+        )
+        Switch(
+             checked = NotesThemeSettings.isDarkThemeEnabled,
+                onCheckedChange = { NotesThemeSettings.isDarkThemeEnabled = it },
+                modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp)
+                        .align(alignment = Alignment.CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LightDarkThemeItemPreview() {
+    NotesTheme {
+        LightDarkThemeItem()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
